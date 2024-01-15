f17ac07faf8982860c4094ba9b144fb0738202a7
Traian Boboc <traian.boboc@ness.com>
2024-01-15 11:51:02 +0200

    FLK-1186 code review refactor

--- long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
+++ long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
@@ -99,7 +99,7 @@ public class LongRidesExercise {
     @VisibleForTesting
     public static class AlertFunction extends KeyedProcessFunction<Long, TaxiRide, Long> {
 
-        private ValueState<TaxiRide> rideState;
+        private transient ValueState<TaxiRide> rideState;
 
         @Override
         public void open(Configuration config) {
